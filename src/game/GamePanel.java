eb46a4196ce9b9681a0712b0c2ce96a5a3edecd1
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-08-03 15:58:52 +0300

    Initial commit

---
+++ src/game/GamePanel.java
@@ -0,0 +1,53 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics graphics) {
+
+    }
+
+    public void draw(Graphics graphics) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics graphics) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent event) {
+
+        }
+    }
+}
